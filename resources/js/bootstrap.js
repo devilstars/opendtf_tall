762b816d9913632e421f33ebc5a5ba518f8f5422
DevilStars <mr.devilstar@gmail.com>
2020-11-24 13:42:02 +0200

    spurce (alpine.js state manager) added

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,3 +1,4 @@
+import './state/spruce';
 import 'alpinejs';
 
 /**
---
+++ resources/js/state/spruce.js
@@ -0,0 +1,9 @@
+import Spruce from '@ryangjchandler/spruce'
+
+/* example */
+// Spruce.store('modal02', {
+//     open: false,
+// });
+/* end of example */
+
+export default Spruce
