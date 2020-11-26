ac44d4595464454c4196a6569039b4be204af86c
DevilStars <mr.devilstar@gmail.com>
2020-11-26 14:48:32 +0200

    basic post feed layout

---
+++ resources/views/components/icons/post-favorites.blade.php
@@ -0,0 +1,6 @@
+<svg class="w-6 h-6 fill-current" version="1.1" xmlns="http://www.w3.org/2000/svg"
+     xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+     viewBox="0 0 251.986 251.986" xml:space="preserve">
+    <path d="M200.838,42.271V15H51.148v208.014l74.845-52.908l74.845,52.908V78.488h15v173.499l-89.845-63.511l-89.845,63.511V0h179.689
+        v57.271H70.327v-15H200.838z"/>
+</svg>
--- resources/views/livewire/pages/home.blade.php
+++ resources/views/livewire/pages/home.blade.php
@@ -1,24 +1,4 @@
 <div class="mt-5">
     <livewire:modules.post-sort />
-{{--    <sort-tool/>--}}
-{{--    <template v-for="(item, index) in getPosts.data">--}}
-{{--        <post-item :key="'post_' + index + '_' + item.id"--}}
-{{--                   :data="item" />--}}
-{{--    </template>--}}
-{{--    <template v-if="!getLoading">--}}
-{{--        <client-only>--}}
-{{--            <infinite-loading @distance="1" @infinite="infiniteLoader">--}}
-{{--                <div slot="no-more" class="my-5">--}}
-{{--            <span class="my-5 text-gray-500">--}}
-{{--                Больше нет записей :)--}}
-{{--            </span>--}}
-{{--                </div>--}}
-{{--                <div slot="no-results">--}}
-{{--            <span class="my-5 text-gray-500">--}}
-{{--                Нет данных :/--}}
-{{--            </span>--}}
-{{--                </div>--}}
-{{--            </infinite-loading>--}}
-{{--        </client-only>--}}
-{{--    </template>--}}
+    <livewire:modules.post-feed />
 </div>
