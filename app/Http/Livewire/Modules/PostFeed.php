5fd08fdfd347e5e0d5f8f20017497f28e497149b
DevilStars <mr.devilstar@gmail.com>
2020-11-26 16:39:10 +0200

    Post feed - infinite scroll as proof of concept (works but needs to be refactored)

--- app/Http/Livewire/Modules/PostFeed.php
+++ app/Http/Livewire/Modules/PostFeed.php
@@ -9,13 +9,30 @@ class PostFeed extends Component
 {
     public $items;
     public $skip = 0; // how much items must be skipped, 0 by default
+    public $perPage = 10;
+
+    protected $listeners = ['loadMore' => 'incrementSkip'];
+
+
+    public function incrementSkip()
+    {
+        $this->skip = $this->skip + $this->perPage;
+    }
 
     public function render()
     {
-        try {
-            $this->items = app()->make('PostService')->getAllPosts($this->skip, 10);
-        } catch (BindingResolutionException $e) {
-            abort (404);
+        $loadMore = null;
+        if (!$this->skip) {
+            $this->items = app()->make('PostService')->getAllPosts(0, 10);
+        }
+
+        if ($this->skip) {
+            try {
+                $loadMore = app()->make('PostService')->getAllPosts($this->skip, $this->perPage);
+            } catch (BindingResolutionException $e) {}
+            $this->items = $this->items->concat($loadMore);
+//            dump($this->items);
+//            dump($loadMore);
         }
 
         return view('livewire.modules.post-feed');
--- resources/views/livewire/modules/post-feed.blade.php
+++ resources/views/livewire/modules/post-feed.blade.php
@@ -71,8 +71,8 @@
         <script type="text/javascript">
             window.onscroll = function(ev) {
                 if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-                    // window.livewire.emit('load-more');
-                    alert('you are here');
+                    Livewire.emit('loadMore');
+                    // alert('you are here');
                 }
             };
         </script>
