4835422cc16d61b789a8103ecf71749c53f11561
DevilStars <mr.devilstar@gmail.com>
2021-06-10 14:16:05 +0300

    keeping things up to date

--- resources/views/components/modules/dashboard/main-menu.blade.php
+++ resources/views/components/modules/dashboard/main-menu.blade.php
@@ -1,3 +1,4 @@
+{{-- TODO make it responsive --}}
 <div class="grid grid-cols-3 gap-4">
     @foreach (array_fill(0, 4, '') as $item)
         <x-modules.dashboard.menu-item :title="__('front.users')" link="#" />
